ab516fc6bfb66f448eaefb8f3ac6d5c128128511
foxyu0 <foxyu00@outlook.com>
2024-04-02 00:59:23 +0800

    init

---
+++ src/main/java/com/code/codeu12/CodeU12Application.java
@@ -0,0 +1,13 @@
+package com.code.codeu12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeU12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeU12Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/code/codeu12/CodeU12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.codeu12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeU12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
